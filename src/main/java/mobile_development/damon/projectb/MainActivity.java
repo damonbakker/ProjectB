1931b7128d6c558e2764f5b2b41da4cdd66431ef
damon bakker <damon.bakker@hotmail.com>
2015-06-24 00:58:36 +0200

    Animation login

--- src/main/java/mobile_development/damon/projectb/MainActivity.java
+++ src/main/java/mobile_development/damon/projectb/MainActivity.java
@@ -1,21 +1,126 @@
 package mobile_development.damon.projectb;
 
-import android.support.v7.app.ActionBarActivity;
+import android.animation.Animator;
+import android.graphics.Color;
+import android.text.Layout;
+import android.util.Log;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    public int counter = 0;
+    public boolean working = false;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {   /*Load the parent activity and any saved instances if any are saved*/
         super.onCreate(savedInstanceState);
+        /*Load layout*/
         setContentView(R.layout.activity_main);
+
 
         /*start*/
     }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        if (working == false)
+        {
+            working = true;
+
+
+        int x = (int)event.getX();
+        int y = (int)event.getY();
+
+        final RelativeLayout mainlayout = (RelativeLayout) findViewById(R.id.main_layout);
+        final RelativeLayout registerlayout = (RelativeLayout) findViewById(R.id.register_layout);
+
+
+        int finalradius = Math.max(mainlayout.getWidth(), mainlayout.getHeight());
+
+
+        switch (event.getAction())
+        {
+            case MotionEvent.ACTION_DOWN:
+                //pressed
+                String stringx = String.valueOf(x);
+                String stringy = String.valueOf(y);
+
+                Log.i("float x =", stringx);
+                Log.i("float y =", stringy);
+
+
+
+                Animator anim= ViewAnimationUtils.createCircularReveal(registerlayout, x, y, 0, finalradius);
+                anim.setDuration(700);
+
+
+
+                anim.addListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animation) {
+                        registerlayout.setVisibility(View.VISIBLE);
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+
+                        Log.i("malin", "klaar");
+                        working = false;
+
+                        mainlayout.setBackgroundColor(Color.MAGENTA);
+                        counter++;
+                        Log.i("complete", "true");
+
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+                        Log.i("cancel", "true");
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animation) {
+
+                    }
+                });
+
+
+                if (counter == 1){
+                    registerlayout.setVisibility(View.INVISIBLE);
+                    registerlayout.setBackgroundColor(Color.YELLOW);
+                }
+
+
+
+                anim.start();
+
+
+
+                return true;
+
+            case MotionEvent.ACTION_UP:
+                //released
+                return true;
+        }
+        }
+        return super.onTouchEvent(event);
+
+    }
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
