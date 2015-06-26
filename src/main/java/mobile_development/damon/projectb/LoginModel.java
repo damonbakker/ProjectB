1d6ee7a1a16e57b06a4dc93e82a048142bdcc1ec
damon bakker <damon.bakker@hotmail.com>
2015-06-26 02:43:26 +0200

    Added icons by malin, Golor generated fixed REQUIRES new colors . + Animation still has a bug but won't be fixed till google fixes theirs

--- src/main/java/mobile_development/damon/projectb/LoginModel.java
+++ src/main/java/mobile_development/damon/projectb/LoginModel.java
@@ -30,7 +30,8 @@ public class LoginModel extends AppCompatActivity
     public List listPalette = new ArrayList<String>();
     public boolean Generator_initialised = false;
     public String current_background_color;
-
+    public boolean animbgstate = false;
+    public boolean animbgstate2 = false;
 
 
 
@@ -71,6 +72,7 @@ public class LoginModel extends AppCompatActivity
 
     public void LoginBackgroundChange(MotionEvent event,RelativeLayout mainlayout1,RelativeLayout registerlayout1)
     {
+
         final RelativeLayout mainlayout = mainlayout1;
         final RelativeLayout registerlayout = registerlayout1;
 
@@ -83,8 +85,10 @@ public class LoginModel extends AppCompatActivity
             if (!countdown_state)
             {
                 countdown_state = true;
-                new CountDownTimer(600, 1000)
+
+                new CountDownTimer(500, 1000)
                 {
+
                     public void onTick(long millisUntilFinished)
                     {
 
@@ -92,14 +96,17 @@ public class LoginModel extends AppCompatActivity
 
                     public void onFinish()
                     {
+                        animbgstate = false;
+
                         countdown_state = false;
                         animation_state = false;
-
+                        Log.i("test","COUNTDOWN KLAAR");
 
                     }
                 }.start();
             }
 
+            Log.i("test","ANIMATIE BOUWEN NU");
 
             //get pressed location(coordinates)
             int x = (int)event.getX();
@@ -110,21 +117,28 @@ public class LoginModel extends AppCompatActivity
 
             //if (new_color == ((PaintDrawable)))
 
-            while (new_color == current_background_color);
+            //Prevebt sane background
+            /*while (new_color == current_background_color);
             {
                 new_color = Color_Generator();
-            }
+            }*/
 
 
             current_background_color = new_color;
-
-            registerlayout.setBackgroundColor(Color.parseColor(new_color));
-
+            Log.i("test",new_color);
+            if (!animbgstate && !animbgstate2)
+            {
+                registerlayout.setBackgroundColor(Color.parseColor(new_color));
+            }
+            Log.i("test","COLORCHAGNE");
 
 
             //get the entire field radius of the view
             int finalradius = Math.max(mainlayout.getWidth(), mainlayout.getHeight());
 
+
+            if (!animbgstate && !animbgstate2)
+            {
 
 
 
@@ -134,7 +148,9 @@ public class LoginModel extends AppCompatActivity
                     //pressed
 
                     //create reveal animation with parameters
-                    Animator anim= ViewAnimationUtils.createCircularReveal(registerlayout, x, y, 0, finalradius);
+
+                        Animator anim = ViewAnimationUtils.createCircularReveal(registerlayout, x, y, 0, finalradius);
+
                     anim.setDuration(animation_duration);
 
 
@@ -151,9 +167,12 @@ public class LoginModel extends AppCompatActivity
                         @Override
                         public void onAnimationEnd(Animator animation)
                         {
+
                             //animation has ended
-                            mainlayout.setBackgroundColor(Color.MAGENTA);
+                            animbgstate2 = false;
                             counter++;
+                            Log.i("test","ANIMATIE EINDE");
+                            mainlayout.setBackgroundColor(Color.parseColor(current_background_color));
 
 
 
@@ -172,22 +191,24 @@ public class LoginModel extends AppCompatActivity
                         }
                     });
 
-                    if (counter == 1)
+                   /* if (counter == 1)
                     {
                         String malin = Color_Generator();
                         Log.i("NEKEN",malin);
                         registerlayout.setVisibility(View.INVISIBLE);
                         registerlayout.setBackgroundColor(Color.YELLOW);
-                    }
+                    }*/
 
                     //start animation
                     anim.start();
+                    animbgstate2=true;
 
                     break; //end switch ACTION_DOWN
 
                 case MotionEvent.ACTION_UP:
                     //released
                     break; //end switch ACTION_UP
+            }
             }
         }
     }
