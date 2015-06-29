28c81f90f964738794a89c88d2a667fb9483df07
damon bakker <damon.bakker@hotmail.com>
2015-06-29 13:17:28 +0200

    removed unnecessary stuff

--- src/main/java/mobile_development/damon/projectb/LoginActivity.java
+++ src/main/java/mobile_development/damon/projectb/LoginActivity.java
@@ -11,7 +11,8 @@ import android.view.MotionEvent;
 import android.widget.RelativeLayout;
 
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity
+{
 
     public boolean touch_state = false;
     public boolean bugfix_state = false;
@@ -76,27 +77,4 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
